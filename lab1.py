a378ada065492516388c002e022166f39595cf3a
Siri0Keeton <fyanuck@gmail.com>
2021-02-18 19:20:04 +0200

    Лабораторна робота №1

--- lab1.py
+++ lab1.py
@@ -31,19 +31,19 @@ ten_intervals = []
 stat_densities = []
 P_list = []
 
+
 def get_Tcp():
     return sum(hour_table) / len(hour_table)
+
 
 def get_T(gamma):
     global interval_len, stat_densities, ten_intervals, P_list
     interval_len = (sorted_table[-1] - sorted_table[0]) / 10
 
-    #ten_intervals = []
     for i in range(0, 10):
         ten_intervals.append([a for a in sorted_table if (i * interval_len <= a <= (i + 1) * interval_len)])
 
     stat_densities = [len(interval) / (len(sorted_table) * interval_len) for interval in ten_intervals]
-    #P_list = []
     area_sum = 1
     for i in range(10):
         P_list.append(area_sum)
@@ -60,6 +60,7 @@ def get_T(gamma):
     T = index_more + interval_len * d
     return T
 
+
 def p_unfail(time):
     time = 275
     Sum = 1
@@ -69,51 +70,20 @@ def p_unfail(time):
     Sum -= stat_densities[whole_intervals] * (time % interval_len)
     return Sum
 
+
 def fail_freq(time):
     f = stat_densities[int(time // interval_len)]
     p = p_unfail(time)
     return f / p
 
+
 # 1. Середній наробіток до відмови Tср
-#Tcp = sum(hour_table) / len(hour_table)
 print("Середній наробіток до відмови Tср:", get_Tcp())
 
 # 2. γ-відсотковий наробіток на відмову Tγ при γ = 0.62
-# interval_len = (sorted_table[-1] - sorted_table[0]) / 10
-# print("Interval:", interval_len)
-#
-# ten_intervals = []
-# for i in range(0, 10):
-#     print("Now looking between", i * interval_len, "and", (i + 1) * interval_len)
-#     ten_intervals.append([a for a in sorted_table if (i * interval_len <= a <= (i + 1) * interval_len)])
-#
-# stat_densities = [len(interval) / (len(sorted_table) * interval_len) for interval in ten_intervals]
-# print(stat_densities)
-# P_list = []
-# area_sum = 1
-# for i in range(10):
-#     P_list.append(area_sum)
-#     print(i, area_sum)
-#     area_sum -= stat_densities[i] * interval_len
-#
-# gamma = 0.62
-# p_less = max([p for p in P_list if p < gamma])
-# p_more = min([p for p in P_list if p > gamma])
-#
-# index_less = P_list.index(p_less)
-# index_more = P_list.index(p_more)
-#
-# d = (p_more - gamma) / (p_more - p_less)
-# T = index_more + interval_len * d
 print("γ-відсотковий наробіток на відмову Tγ при γ = 0.62:", get_T(gamma))
 
 # 3. ймовірність безвідмовної роботи на час 275 годин
-# time = 275
-# Sum = 1
-# whole_intervals = time // interval_len
-# for i in range(whole_intervals):
-#     Sum -= stat_densities[i] * interval_len
-# Sum -= stat_densities[whole_intervals] * (time % interval_len)
 print("ймовірність безвідмовної роботи на час 275 годин:", p_unfail(time1))
 
 # 4. інтенсивність відмов на час 648 годин
