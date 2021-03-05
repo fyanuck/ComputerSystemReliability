3213838d6790f34868624e680193b8fd9fa45a5e
Siri0Keeton <fyanuck@gmail.com>
2021-03-05 13:00:37 +0200

    Исправлено второе задание

--- lab1.py
+++ lab1.py
@@ -3,17 +3,18 @@
 
 # Вхідна вибірка наробітків до відмови (у годинах)
 
-hour_table = [233, 303, 81, 129, 200, 82, 115, 228, 64, 17,
-              67, 648, 29, 39, 210, 10, 94, 465, 135, 312,
-              606, 698, 15, 764, 32, 45, 54, 13, 116, 24,
-              477, 16, 841, 95, 3, 79, 118, 208, 9, 59, 171,
-              295, 78, 67, 38, 57, 91, 18, 39, 324, 416,
-              270, 114, 25, 675, 287, 374, 119, 227, 5,
-              109, 94, 171, 226, 183, 350, 27, 64, 433, 88,
-              167, 152, 159, 319, 8, 162, 36, 488, 65, 77,
-              307, 522, 140, 65, 355, 482, 180, 29, 342,
-              233, 117, 182, 184, 113, 86, 630, 476, 136,
-              397, 66]
+hour_table = [644, 1216, 2352, 1386, 1280, 903, 607, 2068,
+4467, 835, 313, 555, 307, 508, 1386, 2895, 583,
+292, 5159, 1107, 181, 18, 1247, 125, 1452, 4211,
+890, 659, 1602, 2425, 214, 68, 21, 1762, 1118,
+45, 1803, 1187, 2154, 19, 1122, 278, 1622, 702,
+1396, 694, 45, 1739, 3483, 1334, 1852, 96, 173,
+7443, 901, 2222, 4465, 18, 1968, 1426, 1424,
+1146, 435, 1390, 246, 578, 281, 455, 609, 854,
+436, 1762, 444, 466, 1934, 681, 4539, 164, 295,
+1644, 711, 245, 740, 18, 474, 623, 462, 605, 187,
+106, 793, 92, 296, 226, 63, 246, 446, 2234, 2491,
+315]
 
 # Завдання за варіантом:
 # 1. Середній наробіток до відмови Tср,
@@ -21,9 +22,9 @@ hour_table = [233, 303, 81, 129, 200, 82, 115, 228, 64, 17,
 # 3. ймовірність безвідмовної роботи на час 275 годин,
 # 4. інтенсивність відмов на час 648 годин
 
-gamma = 0.62
-time1 = 275
-time2 = 648
+gamma = 0.9
+time1 = 2000
+time2 = 2000
 
 sorted_table = sorted(hour_table)
 interval_len = 0
@@ -43,7 +44,8 @@ def get_Tcp():
 
 def get_T(gamma):
     global interval_len, stat_densities, ten_intervals, P_list
-    interval_len = (sorted_table[-1] - sorted_table[0]) / 10
+    #interval_len = (sorted_table[-1] - sorted_table[0]) / 10
+    interval_len = (max(sorted_table) - min(sorted_table)) / 10
 
     for i in range(0, 10):
         ten_intervals.append([a for a in sorted_table if (i * interval_len <= a <= (i + 1) * interval_len)])
@@ -57,11 +59,13 @@ def get_T(gamma):
     p_less = max([p for p in P_list if p < gamma])
     p_more = min([p for p in P_list if p > gamma])
 
-    #index_less = P_list.index(p_less)
+    index_less = P_list.index(p_less)
     index_more = P_list.index(p_more)
 
-    d = (p_more - gamma) / (p_more - p_less)
-    T = index_more + interval_len * d
+    #interval_len = 744.3
+
+    d = (p_less - gamma) / (p_less - p_more)
+    T = (interval_len * index_less) - interval_len * d
     return T
 
 
